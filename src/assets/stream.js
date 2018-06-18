80002e2549dc9987f42dc55ba17c4ef07185452a
kkevindev <34477826+kkevindev@users.noreply.github.com>
2018-06-18 15:33:36 +0200

    changed url :alien:

--- src/assets/stream.js
+++ src/assets/stream.js
@@ -1,12 +1,11 @@
 function loadStream(id){
   if (flvjs.isSupported()) {
-      var videoElement = document.getElementById(id);
-      var flvPlayer = flvjs.createPlayer({
-          type: 'flv',
-          url: 'ws://145.49.34.4:8000/live/kevin.flv'
-      });
-      flvPlayer.attachMediaElement(videoElement);
-      flvPlayer.load();
-      flvPlayer.play();
+    var flvPlayer = flvjs.createPlayer({
+      type: 'flv',
+      url: 'http://145.49.34.4:8000/live/stream.flv'
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    flvPlayer.play();
   }
 }
